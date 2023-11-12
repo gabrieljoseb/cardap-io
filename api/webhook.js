62c3666f23465f80309f391f0f9f6547b0ae7250
= <=>
2023-11-12 01:13:09 -0300

    removed async when calling kitchen endpoints

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,6 @@
 import axios from 'axios/dist/node/axios.cjs';
 
-const webhookHandler = async (req, res) => {
+const webhookHandler = (req, res) => {
   if (req.method === 'POST') {
     try {
       const { type, data } = req.body;
@@ -8,24 +8,29 @@ const webhookHandler = async (req, res) => {
       if (type === 'payment') {
         const { id } = data;
         // Rota para receber os detalhes da transação.
-        const paymentResponse = await axios.get(`https://api.mercadopago.com/v1/payments/${id}`, {
+        axios.get(`https://api.mercadopago.com/v1/payments/${id}`, {
           headers: {
             'Authorization': `Bearer ${process.env.MERCADO_PAGO_ACCESS_TOKEN}`
           }
-        });
+        }).then(async response => {
+          const paymentData = response.data;
+          //const payerData = paymentData.payer;
+          const items = paymentData.additional_info.items;
 
-        const paymentData = paymentResponse.data;
-        const items = paymentData.additional_info.items;
-
-        // As duas chamadas abaixo podem ser feitas em paralelo
-        await Promise.all([
+          // Inserir os dados do pagador na tabela 'pedidos'.
           axios.post('https://kitchen-io.vercel.app/api/orders', {
             numero_transacao: id,
             nome_cliente: 'Test User',
+            //nome_cliente: payerData.first_name + ' ' + payerData.last_name,
             status: 'Pendente',
-          }),
+          }).catch(error => console.log('[POST] api/orders error: ', error));
+
+          // Inserir os itens do pedido na tabela 'pedidos_itens'.
           axios.post('https://kitchen-io.vercel.app/api/orders_itens', { items: items, id: id })
-        ]);
+            .catch(error => console.log('[POST] api/orders_itens error: ', error));
+        }).catch(error => {
+          console.error('Erro na requisição:', error);
+        });
       }
 
       console.log('Webhook recebido:', req.body);
