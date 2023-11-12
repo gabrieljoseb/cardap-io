e73b824b539e484f1ef503d94b653a95043227fb
= <=>
2023-11-12 00:09:52 -0300

    performance enhancement

--- api/webhook.js
+++ api/webhook.js
@@ -1,54 +1,43 @@
 import axios from 'axios';
 
 const webhookHandler = async (req, res) => {
-
   if (req.method === 'POST') {
     try {
       const { type, data } = req.body;
 
-      // Verificar o tipo do webhook e tratar os dados conforme necessário.
-      // Por exemplo, para um pagamento, você pode querer extrair informações da transação.
       if (type === 'payment') {
         const { id } = data;
         // Rota para receber os detalhes da transação.
-        axios.get(`https://api.mercadopago.com/v1/payments/${id}`, {
+        const paymentResponse = await axios.get(`https://api.mercadopago.com/v1/payments/${id}`, {
           headers: {
             'Authorization': `Bearer ${process.env.MERCADO_PAGO_ACCESS_TOKEN}`
           }
-        }).then(async response => {
-          const paymentData = response.data;
-          //const payerData = paymentData.payer;
-          const items = paymentData.additional_info.items;
+        });
 
-          // Inserir os dados do pagador na tabela 'pedidos'.
+        const paymentData = paymentResponse.data;
+        const items = paymentData.additional_info.items;
+
+        // As duas chamadas abaixo podem ser feitas em paralelo
+        await Promise.all([
           axios.post('https://kitchen-io.vercel.app/api/orders', {
             numero_transacao: id,
             nome_cliente: 'Test User',
-            //nome_cliente: payerData.first_name + ' ' + payerData.last_name,
             status: 'Pendente',
-          }).catch(error => console.log('[POST] api/orders error: ', error));
-
-          // Inserir os itens do pedido na tabela 'pedidos_itens'.
+          }),
           axios.post('https://kitchen-io.vercel.app/api/orders_itens', { items: items, id: id })
-            .catch(error => console.log('[POST] api/orders_itens error: ', error));
-        }).catch(error => {
-          console.error('Erro na requisição:', error);
-        });
+        ]);
       }
-      // Log e resposta de sucesso.
+
       console.log('Webhook recebido:', req.body);
       res.status(200).send('Webhook recebido com sucesso');
     } catch (error) {
-      // Log e resposta de erro.
       console.error('Erro ao processar webhook:', error);
       res.status(500).send('Erro interno do servidor');
     }
   } else {
-    // Permitir apenas métodos POST.
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 };
 
-// Exportar a função nomeada como padrão.
 export default webhookHandler;
