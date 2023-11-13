52732473e125381668807dcf85ed5ff694de32f4
= <=>
2023-11-13 02:25:05 -0300

    firebase ui

--- src/routes/login/Login.js
+++ src/routes/login/Login.js
@@ -1,63 +1,49 @@
-// Login.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { firebaseConfig } from '../../config/firebase.config';
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, FacebookAuthProvider, signInWithPopup } from "firebase/auth";
-import { getAnalytics, logEvent } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import firebaseui from 'firebaseui';
+import 'firebaseui/dist/firebaseui.css';
 
 // Inicializar o Firebase com as configurações
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const analytics = getAnalytics(app);
-const googleProvider = new GoogleAuthProvider();
-const facebookProvider = new FacebookAuthProvider();
 
-class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            user: null
+const uiConfig = {
+  signInSuccessUrl: '/', // URL para redirecionar após o login
+  signInOptions: [
+    // Opções de provedores de login
+    getAuth.GoogleAuthProvider.PROVIDER_ID,
+    getAuth.FacebookAuthProvider.PROVIDER_ID,
+  ],
+};
+
+function Login({ setUser }) {
+  useEffect(() => {
+    // Inicializar a instância da Firebase UI
+    const ui = new firebaseui.auth.AuthUI(auth);
+
+    // Iniciar a Firebase UI com a configuração
+    ui.start('#firebaseui-auth-container', uiConfig);
+
+    // Lidar com a atualização do estado do usuário após o login
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        const userInfo = {
+          nome: user.displayName,
+          email: user.email,
         };
-    }
+        setUser(userInfo);
+        localStorage.setItem('user', JSON.stringify(userInfo));
+      }
+    });
+  }, [setUser]);
 
-    handleLogin = async (provider) => {
-        try {
-            const result = await signInWithPopup(auth, provider);
-            const user = result.user;
-
-            // Extrair informações do usuário
-            const userInfo = {
-                nome: user.displayName,
-                email: user.email,
-            };
-
-            // Atualizar o estado com as informações do usuário
-            this.setState({ user: userInfo });
-
-            // Passar as informações para o componente pai
-            this.props.setUser(userInfo);
-
-            // Enviando evento para o Firebase Analytics
-            logEvent(analytics, 'login', { method: provider.providerId });
-
-            // Armazenar as informações do usuário no localStorage para persistência entre sessões
-            localStorage.setItem('user', JSON.stringify(userInfo));
-        } catch (error) {
-            console.error(error);
-            // Enviar evento de erro para o Firebase Analytics
-            logEvent(analytics, 'login_failure', { error: error.message });
-        }
-    };
-
-    render() {
-        return (
-            <div>
-                <button onClick={() => this.handleLogin(googleProvider)}>Login com Google</button>
-                <button onClick={() => this.handleLogin(facebookProvider)}>Login com Facebook</button>
-                {this.state.user && <div>Bem-vindo, {this.state.user.nome}</div>}
-            </div>
-        );
-    }
+  return (
+    <div>
+      <div id="firebaseui-auth-container"></div>
+    </div>
+  );
 }
 
 export default Login;
