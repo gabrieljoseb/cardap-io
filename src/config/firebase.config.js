8988c37983a6abf01426cba9dc9514a3058572bd
= <=>
2023-11-12 21:10:08 -0300

    trying hardcoded firebase config

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,9 +1,16 @@
 export const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    // apiKey: process.env.FIREBASE_API_KEY,
+    // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    // projectId: process.env.FIREBASE_PROJECT_ID,
+    // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    // appId: process.env.FIREBASE_APP_ID,
+    // measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyDyVrL5XvTm2OJiYemlWiag3AtK76xXGT0",
+    authDomain: "cardap-io-69ddb.firebaseapp.com",
+    projectId: "cardap-io-69ddb",
+    storageBucket: "cardap-io-69ddb.appspot.com",
+    messagingSenderId: "691773711539",
+    appId: "1:691773711539:web:6dcde72c487a09446afcc6",
+    measurementId: "G-SX69ZNBEHR"
 };
