806bd2c140ad2c854ae303546cef523373340848
= <=>
2023-11-13 02:32:08 -0300

    fixing build error

--- src/routes/login/Login.js
+++ src/routes/login/Login.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
 import { firebaseConfig } from '../../config/firebase.config';
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import firebaseui from 'firebaseui';
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
+import * as firebaseui from 'firebaseui';
 import 'firebaseui/dist/firebaseui.css';
 
 // Inicializar o Firebase com as configurações
@@ -13,18 +13,21 @@ const uiConfig = {
   signInSuccessUrl: '/', // URL para redirecionar após o login
   signInOptions: [
     // Opções de provedores de login
-    getAuth.GoogleAuthProvider.PROVIDER_ID,
-    getAuth.FacebookAuthProvider.PROVIDER_ID,
+    GoogleAuthProvider.PROVIDER_ID,
+    FacebookAuthProvider.PROVIDER_ID,
   ],
 };
 
 function Login({ setUser }) {
   useEffect(() => {
-    // Inicializar a instância da Firebase UI
-    const ui = new firebaseui.auth.AuthUI(auth);
-
-    // Iniciar a Firebase UI com a configuração
-    ui.start('#firebaseui-auth-container', uiConfig);
+    // Verifica se a instância da Firebase UI já foi inicializada para evitar duplicação
+    if (firebaseui.auth.AuthUI.getInstance()) {
+      const ui = firebaseui.auth.AuthUI.getInstance();
+      ui.start('#firebaseui-auth-container', uiConfig);
+    } else {
+      const ui = new firebaseui.auth.AuthUI(auth);
+      ui.start('#firebaseui-auth-container', uiConfig);
+    }
 
     // Lidar com a atualização do estado do usuário após o login
     auth.onAuthStateChanged((user) => {
@@ -37,6 +40,12 @@ function Login({ setUser }) {
         localStorage.setItem('user', JSON.stringify(userInfo));
       }
     });
+
+    // Limpar a UI ao desmontar o componente
+    return () => {
+      const ui = firebaseui.auth.AuthUI.getInstance();
+      if (ui) ui.delete();
+    };
   }, [setUser]);
 
   return (
