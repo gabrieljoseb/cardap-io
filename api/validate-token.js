c03de9d6071970771ba85720da68bbae16dbff9b
= <=>
2023-11-15 14:24:16 -0300

    validate-token refactor

--- api/validate-token.js
+++ api/validate-token.js
@@ -1,19 +1,20 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-});
+import pool from './database';
 
 export default async function handler(req, res) {
     const { token } = req.query;
 
-    // Verificar se o token existe
-    const tokenResult = await pool.query('SELECT * FROM mesa_tokens WHERE token = $1', [token]);
-    if (tokenResult.rowCount === 1) {
-        // Token existe, permitir acesso
-        res.status(200).json({ message: 'Acesso permitido', mesaId: tokenResult.rows[0].mesa_id });
-    } else {
-        // Token não existe ou inválido
-        res.status(401).json({ message: 'Acesso negado' });
+    try {
+        // Verificar se o token existe
+        const tokenResult = await pool.query('SELECT * FROM mesa_tokens WHERE token = $1', [token]);
+        if (tokenResult.rowCount === 1) {
+            // Token existe, permitir acesso
+            res.status(200).json({ message: 'Acesso permitido', mesaId: tokenResult.rows[0].mesa_id });
+        } else {
+            // Token não existe ou inválido
+            res.status(401).json({ message: 'Acesso negado' });
+        }
+    } catch (error) {
+        console.error('Database error:', error);
+        res.status(500).json({ message: 'Erro interno do servidor' });
     }
 }
